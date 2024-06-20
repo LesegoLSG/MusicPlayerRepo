1db50ce078bf3ec96a00d15771b8a626b2e4b8bc
LesegoLSG <lesegomhlongo78@gmail.com>
2024-06-20 01:20:09 -0500

    Included a navbar and outlets

--- MusicPlayerProject/src/Pages/HomeLandingPage.jsx
+++ MusicPlayerProject/src/Pages/HomeLandingPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import SideBar from "../Components/SideBar/SideBar";
+import Player from "../Components/Player/Player";
+import { Outlet } from "react-router-dom";
+import NavBar from "../Components/NavBar/NavBar";
 
 const HomeLandingPage = () => {
-  return <div>HomeLandingPage</div>;
+  return (
+    <section className="h-screen bg-black ">
+      <div className="h-[90%] flex flex-1">
+        <SideBar />
+        <div className="flex-1">
+          <NavBar />
+          <Outlet />
+        </div>
+      </div>
+      <Player />
+    </section>
+  );
 };
 
 export default HomeLandingPage;
