4bc7ec413254a2fbd168e285ebb60162a4c68b75
LesegoLSG <lesegomhlongo78@gmail.com>
2024-07-27 18:55:56 -0500

    Added a logo and home navigation

--- MusicPlayerProject/src/Components/NavBar/NavBarTop.jsx
+++ MusicPlayerProject/src/Components/NavBar/NavBarTop.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Logo2 from "../../assets/Logo/Logo2.png";
+import { useNavigate } from "react-router-dom";
 
+//NavBar with only the logo
 const NavBarTop = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full min-h-14 flex justify-center items-center p-2">
       {/* Logo */}
       <div className="max-w-screen-2xl container h-auto flex justify-start items-center bg-neutral-900 px-4 py-2 rounded-md">
-        <img src={Logo2} alt="Logo2" className="w-28 md:w-36" />
+        <img
+          src={Logo2}
+          alt="Logo2"
+          className="w-28 md:w-36 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
       </div>
     </div>
   );
