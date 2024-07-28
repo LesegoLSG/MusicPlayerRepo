511570a23a28554d0fa160173a4068824fd40b6e
LesegoLSG <lesegomhlongo78@gmail.com>
2024-07-27 19:03:06 -0500

    Added a color to the themes

--- MusicPlayerProject/tailwind.config.js
+++ MusicPlayerProject/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "hoverColor":"#446a6e"
+      }
+    },
   },
   plugins: [],
 }
